b84fb50bdb5719d8bb7bb68be80a355be1856aab
Rashik <rashik.tuladhar@outlook.com>
2016-03-21 08:06:10 +0545

    resource routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-
-  get 'tasks/create'
-
-  get 'tasks/update'
-
-  get 'tasks/delete'
+  resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
