dcb2ef50ef2cd7345578e01abcce56ecb60500fb
Rashik <rashik.tuladhar@outlook.com>
2016-03-21 08:32:54 +0545

    new controller added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,19 @@
 class TasksController < ApplicationController
   def index
+  	@tasks = Task.all
   end
 
   def create
+
   end
 
   def update
   end
 
   def delete
+  end
+
+  def new
+ 	@task = Task.new 	
   end
 end
